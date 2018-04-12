32d20f651b82816e1713042bf96c5594cc3fee41
haribheri <bheri.bheri@gmail.com>
2018-04-12 18:34:01 +0530

    changes

--- collections/priority_q.cpp
+++ collections/priority_q.cpp
@@ -1,3 +1,39 @@
-#include<iostream>
-#include<stdlib.h>
+#include<bits/stdc++.h>
+#include<priority_queue>
+#include<vector>
 using namespace std;
+
+struct man
+{
+  string name;
+  int priority; 
+};
+
+class comparator
+{
+ public:
+   bool operator()(const man& a, const man& b)
+   {
+        return a.priority<b.priority;
+   }
+};
+
+int main()
+{
+   man arr[5];
+   priority_queue<man, vector<man>, comparator> pq;
+
+   for(int i=0; i<3; i++)
+   {
+     cin>>arr[i].name>>arr[i].priority;
+     pq.push(arr[i]);
+   }
+
+   while (!pq.empty())
+   {
+     cout<<pq.top().name<<" "<<pq.top().priority;
+     pq.pop();
+     cout<<endl;
+   }
+   return 0;
+}
--- priority_queue.cpp
+++ priority_queue.cpp
@@ -1,13 +1,31 @@
 #include<iostream>
 #include<queue>
 using namespace std;
+typedef struct node
+{
+int i,
+char ch;
+float f;
+}node;
 int main()
 {
-priority_queue <int> pq;
-pq.push(-1);
-pq.push(7);
-pq.push(-2);
-pq.push(0);
+priority_queue <node> pq;
+node n1;
+
+node arr[2];
+
+arr[0].i=1;
+arr[0].ch='a';
+arr[0].f=1.0;
+
+
+arr[1].i=2;
+arr[1].ch='b';
+arr[1].f=2.0;
+
+
+pq.push(arr[0]);
+pg.push(arr[1]);
 cout<<"count is "<<pq.size()<<endl;
 return 0;
 }
